ca46857903cfff005b5a180b95d6687bcb6c89e4
Callum McNeilage <31269757+Sunthas515@users.noreply.github.com>
2022-01-18 15:41:19 +1000

    Begin user login

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import fifty from '../resources/50.png';
 
 export default function Home() {
     return (
         <div className="Home">
+            <img name="image" src={fifty} alt="50" style={{padding: '1%', width:'25%', float: "left"}} />
             <h1 style={{textAlign:'left', paddingLeft:'1%', color:'#993366', width: "958px", height: "78px"}}>Welcome to Mallard and Claret Fly Fishers Club Inc</h1>
         </div>
     )
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,48 @@
-import React from 'react';
-import {Button, Input} from 'reactstrap';
-
+import React, {useState} from 'react';
+import {Button, Input, Label} from 'reactstrap';
 export default function Login() {
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
+    const [error, setError] = useState();
+
+    const rmCheck = document.getElementById("rememberMe"),
+        emailInput = document.getElementById("username"),
+        passwordInput = document.getElementById("password");
+
+    function IsRememberMe() {
+        if (rmCheck.checked && emailInput.value !== "" && passwordInput.value !== "") {
+            localStorage.username = emailInput.value;
+            localStorage.password = passwordInput.value;
+            localStorage.checkbox = rmCheck.value;
+            console.log("Remember Me");
+        } else {
+            localStorage.username = "";
+            localStorage.password = "";
+            localStorage.checkbox = "";
+        }
+    }
+
+
+    function getUser() {
+        return fetch('https://110.232.143.7:3306/wb_users', {
+            method: "POST",
+            headers: { accept: "application/json", "Content-Type": "Application/json"},
+            body: JSON.stringify({ username: username, password: password})
+        })
+        .then((res) => res.json())
+        .then((err) => {
+            if (err.error) { setError(err.message); throw new Error(err.message) }
+            else {
+                IsRememberMe()
+                setError("Login successful")
+                localStorage.token = err.token
+            }
+        })
+        .catch(err => {
+
+        })
+    }
+
     return (
         <div className="Login">
             <div className="OutterForm" style={{textAlign:'left', paddingLeft:'1%'}}>
@@ -10,19 +51,20 @@ export default function Login() {
                 <p>Please enter your loginname and password below</p>
                 <div className="InnerForm">
                     <p style={{paddingRight:'1px'}}>Loginname:</p>
-                    <Input/>
+                    <Input type="text" name="username" id="username" onChange={e => setUsername(e.target.value)}/>
                 </div>
                 <div className="InnerForm">
                     <p style={{paddingRight:'12px'}}>Password:</p>
-                    <Input />
+                    <Input type="password" name="password" id="password" onChange={e => setPassword(e.target.value)} />
                 </div>
                 <div className="InnerForm" style={{paddingTop:'1%'}}>
-                    <Input type="checkbox"/>
+                    <Input type="checkbox" value="IsRememberMe" id="rememberMe"/>
                     <p style={{paddingLeft:'1%'}}>Remember Me</p>
                 </div>
                 <div className="InnerForm" style={{paddingTop:'1%'}}>
-                    <Button href="/members">Login</Button>
+                    <Button onClick={getUser}>Login</Button>
                     <Button href="mailto:mallardandclaret@gmail.com?subject=Member reset password&body=Member details here">Reset</Button>
+                    <Label>{error}</Label>
                 </div>
                 <a href="mailto:mallardandclaret@gmail.com?subject=Member forget details&body=Member details here">Forgotten your details?</a>
             </div>
